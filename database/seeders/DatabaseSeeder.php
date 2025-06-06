4b4c52bfe99e4bfdcb1ef3af85fba6621e869a71
phuongtri1909 <tri2003bt@gmail.com>
2025-06-06 08:48:06 +0700

    country seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             //AdminSeeder::class,
-            //CountrySeeder::class,
+            CountrySeeder::class,
             StateSeeder::class,
             CitySeeder::class,
         ]);
